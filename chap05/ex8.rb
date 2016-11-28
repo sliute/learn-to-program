2c5d1a0138d520273fd7dfccf03630e089e110b7
Stefan Liute <stefanliute@gmail.com>
2016-11-28 21:15:29 +0000

    Chapter 05

---
+++ chap05/ex8.rb
@@ -0,0 +1,7 @@
+puts "Your first name, please?"
+fname = gets.chomp
+puts "And your middle name?"
+mname = gets.chomp
+puts "And your last name, too?"
+lname = gets.chomp
+puts "Greeting and pickles, #{fname} #{mname} #{lname}!"
---
+++ chap05/ex9.rb
@@ -0,0 +1,3 @@
+puts "And what\'s your favourite number, if I may ask?"
+number = gets.chomp.to_i
+puts "Oh, but I think #{(number+1).to_s} is bigger, better and favouriter!"
