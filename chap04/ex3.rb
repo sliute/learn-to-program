b685b218bf0636cd33740ba5be2310e49649d0f9
Stefan Liute <stefanliute@gmail.com>
2016-11-28 21:00:16 +0000

    Chapter 04

---
+++ chap04/ex3.rb
@@ -0,0 +1,4 @@
+name = 'Anya Christina Emmanuella Jenkins Harris'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name
+puts 'is a really long name!'
---
+++ chap04/ex5.rb
@@ -0,0 +1,5 @@
+my_own_var = 'just another ' + 'string'
+puts my_own_var
+
+my_own_var = 5 * (1+2)
+puts my_own_var
