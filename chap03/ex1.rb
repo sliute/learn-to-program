c4110e0a73b07c0514f0b8ed0730e99d55006238
Stefan Liute <stefanliute@gmail.com>
2016-11-28 20:48:44 +0000

    Chapter 03

---
+++ chap03/ex1.rb
@@ -0,0 +1,10 @@
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+puts 'I like' + 'apple pie.'
+
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie'
+
+puts 'blink' * 4
---
+++ chap03/ex3.rb
@@ -0,0 +1,16 @@
+puts '12'+ 12
+puts '2' * '5'
+
+puts 'Betty' + 12
+puts 'Fred' * 'John'
+
+puts 'You're swell!'
+
+'
+
+puts 'You\'re swell!'
+
+puts 'You\'re swell!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
