06edc4b8b71d0710f7afb9b8c891d8f19949edda
Stefan Liute <stefanliute@gmail.com>
2016-11-28 21:41:43 +0000

    Chapter 06

---
+++ chap06/ex7.rb
@@ -0,0 +1,3 @@
+puts "What do you want to ask your angry boss?"
+question = gets.chomp
+puts 'WHADDAYA MEAN "' + question.upcase + '"?!? YOU\'RE FIRED!!'
---
+++ chap06/ex8.rb
@@ -0,0 +1,5 @@
+puts "Table of Contents".center(50)
+puts
+puts "Chapter 1:  Getting Started".ljust(30) + "page  1".rjust(20)
+puts "Chapter 2:  Numbers".ljust(30) + "page  9".rjust(20)
+puts "Chapter 3:  Letters".ljust(30) + "page 13".rjust(20)
